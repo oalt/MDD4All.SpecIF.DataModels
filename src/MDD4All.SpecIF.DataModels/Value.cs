435534ed91b7ce766a874c0ef513eef35a2096e5
oalt <oa@lacheweg21.de>
2021-04-15 20:44:01 +0200

    formatting improved

--- src/MDD4All.SpecIF.DataModels/Value.cs
+++ src/MDD4All.SpecIF.DataModels/Value.cs
@@ -9,32 +9,32 @@ using System.Collections.Generic;
 
 namespace MDD4All.SpecIF.DataModels
 {
-	[JsonConverter(typeof(ValueConverter))]
+    [JsonConverter(typeof(ValueConverter))]
     public class Value : SpecIfElement
-	{
-		public Value()
-		{
-			 
-		}
+    {
+        public Value()
+        {
 
-		public Value(string value)
-		{
+        }
+
+        public Value(string value)
+        {
             StringValue = value;
-		}
+        }
 
-		public Value(MultilanguageText value)
-		{
-			MultilanguageText.Add(value);
-		}
+        public Value(MultilanguageText value)
+        {
+            MultilanguageText.Add(value);
+        }
 
-		public static string ToSimpleTextString(object value)
+        public static string ToSimpleTextString(object value)
         {
             string result = "";
-            if(value is string)
+            if (value is string)
             {
                 result = (string)value;
             }
-            else if(value is object[])
+            else if (value is object[])
             {
 
             }
@@ -42,34 +42,34 @@ namespace MDD4All.SpecIF.DataModels
             return result;
         }
 
-		[BsonElement("multilanguageText")]
+        [BsonElement("multilanguageText")]
         [JsonIgnore]
-		public List<MultilanguageText> MultilanguageText { get; set; } = new List<MultilanguageText>();
+        public List<MultilanguageText> MultilanguageText { get; set; } = new List<MultilanguageText>();
 
         [BsonElement("simpleValue")]
         [JsonIgnore]
         public string StringValue { get; set; } = null;
 
 
-		public string ToSimpleTextString()
-		{
-			string result = "";
+        public string ToSimpleTextString()
+        {
+            string result = "";
 
-            if(StringValue != null)
+            if (StringValue != null)
             {
                 result = StringValue;
             }
-            else if(MultilanguageText.Count > 0)
-			{
-				result = MultilanguageText[0].Text;
-			}
+            else if (MultilanguageText.Count > 0)
+            {
+                result = MultilanguageText[0].Text;
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		public override string ToString()
-		{
-			return ToSimpleTextString();
-		}
-	}
+        public override string ToString()
+        {
+            return ToSimpleTextString();
+        }
+    }
 }
