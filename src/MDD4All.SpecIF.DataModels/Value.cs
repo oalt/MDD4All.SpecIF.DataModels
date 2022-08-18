ca0bb2ee45e9a7ef875192187453236b18a128ed
Oliver Alt <oa@lacheweg21.de>
2022-08-18 10:15:10 +0200

    Move helper methods to exenstion classes

--- src/MDD4All.SpecIF.DataModels/Value.cs
+++ src/MDD4All.SpecIF.DataModels/Value.cs
@@ -6,7 +6,6 @@ using MDD4All.SpecIF.DataModels.Converters;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace MDD4All.SpecIF.DataModels
 {
@@ -39,21 +38,6 @@ namespace MDD4All.SpecIF.DataModels
             MultilanguageTexts.Add(value);
         }
 
-        public static string ToSimpleTextString(object value)
-        {
-            string result = "";
-            if (value is string)
-            {
-                result = (string)value;
-            }
-            else if (value is object[])
-            {
-
-            }
-
-            return result;
-        }
-
         [BsonElement("multilanguageTexts")]
         [JsonIgnore]
         public List<MultilanguageText> MultilanguageTexts { get; set; } = new List<MultilanguageText>();
@@ -62,61 +46,5 @@ namespace MDD4All.SpecIF.DataModels
         [JsonIgnore]
         public string StringValue { get; set; } = null;
 
-        [BsonIgnore]
-        [JsonIgnore]
-        public bool IsStringValue
-        {
-            get
-            {
-                return StringValue != null;
-            }
-        }
-
-        [BsonIgnore]
-        [JsonIgnore]
-        public bool IsMultilanguageValue
-        {
-            get
-            {
-                return !(MultilanguageTexts == null || MultilanguageTexts.Count == 0);
-            }
-        }
-
-        public string ToSimpleTextString(string language = "en")
-        {
-            string result = "";
-
-            if (StringValue != null)
-            {
-                result = StringValue;
-            }
-            else if (MultilanguageTexts.Count > 0)
-            {
-                try
-                {
-                    result = MultilanguageTexts.First(mlt => mlt.Language == language).Text;
-
-                    if (result == null)
-                    {
-                        result = MultilanguageTexts[0].Text;
-                    }
-                }
-                catch
-                { }
-                
-            }
-
-            return result;
-        }
-
-        public override string ToString()
-        {
-            return ToSimpleTextString();
-        }
-
-        public string ToString(string language)
-        {
-            return ToSimpleTextString(language);
-        }
     }
 }
